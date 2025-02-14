428d17431345028cd44e8dde3149f57ef7ee0fa2
taniyakeswani <140901769+taniyakeswani@users.noreply.github.com>
2025-02-14 11:19:33 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,16 @@
+class Pattern1 
+{
+	public static void main(String[] args) 
+	{
+		int n = 4;
+		for (int j = 0; j < n ; j++)
+		{
+			for (int i = 0; i < n ; i++ )
+			{
+		          System.out.print(" * ");
+			}
+			      System.out.println();
+		}
+		System.out.println();
+	}
+}
---
+++ Pattern2.java
@@ -0,0 +1,16 @@
+class Pattern2 
+{
+	public static void main(String[] args) 
+	{
+		int n = 4;
+		for(int i = 0; i < n-1; i++)
+		{
+			for (int j = 0 ; j< n; j++ )
+			{
+				System.out.print(" * " );
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+}
