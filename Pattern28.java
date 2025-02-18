37eb050a39c2ec247814b376204f1bac2e347a86
taniyakeswani <140901769+taniyakeswani@users.noreply.github.com>
2025-02-18 14:09:04 +0530

    Add files via upload

---
+++ Pattern28.java
@@ -0,0 +1,19 @@
+class Pattern28 
+{
+	public static void main(String[] args) 
+	{
+/*         TRIANGLE */
+		for (int i = 5; i >= 1 ; i-- )
+		{
+			for (int j = 1; j <= 5-i; j++)
+			{
+				System.out.print("  ");
+			}
+			for (int j = 1; j <= i ; j++ )
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+}
