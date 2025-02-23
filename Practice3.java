8d18e0aa8181ed8c605c78647947e31f6b0b74b7
taniyakeswani <140901769+taniyakeswani@users.noreply.github.com>
2025-02-24 00:03:10 +0530

    Add files via upload

---
+++ Practice3.java
@@ -0,0 +1,14 @@
+class Practice3 
+{
+	public static void main(String[] args) 
+	{
+		for (char i = 'a'; i <= 'e'; i++)
+		{
+			for (char j = 'a'; j <= i; j++)
+			{
+				System.out.print(i + " ");
+			}
+			System.out.println();
+		}
+	}
+}
